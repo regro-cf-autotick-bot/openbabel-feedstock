ccf41412a8d9329d595538cd6f64ae3c15ddaae1
Fredrik Wallner <fredrik@wallner.nu>
2026-06-04 16:54:28 +0200

    Fix standard library headers on OSX

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Configure CMAKE for macOS SDK path to ensure C++ standard library headers are found
+CMAKE_OSX_FLAGS=""
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  if [[ -n "$CONDA_BUILD_SYSROOT" ]]; then
+    CMAKE_OSX_FLAGS="-DCMAKE_OSX_SYSROOT=$CONDA_BUILD_SYSROOT"
+  fi
+fi
+
 cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX \
@@ -7,6 +15,7 @@ cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DPython_EXECUTABLE=$PYTHON \
       -DPYTHON_BINDINGS=ON \
       -DRUN_SWIG=ON \
+      ${CMAKE_OSX_FLAGS} \
       .
 
 make #-j${CPU_COUNT}
